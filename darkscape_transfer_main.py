59f4d2f4b0e7373c37513503fe41aa1f8c449371
Jake 'Donkey" Torrance <jaket1234@hotmail.com>
2016-03-12 05:45:40 +0000

    initial commit.
    initial commit - finished at this point, not much to do.

---
+++ darkscape_transfer_main.py
@@ -0,0 +1,85 @@
+import sys
+from darkscape_transfer_skills import Runescaper, Skill
+from darkscape_transfer_ui import Ui_DarkscapeTransferWindow
+from PyQt5.QtCore import Qt, QRegExp, QCoreApplication
+from PyQt5.QtGui import QRegExpValidator
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+
+class DarkscapeUI(QMainWindow):
+    def __init__(self):
+        super(QMainWindow, self).__init__()
+        
+        self.ui = Ui_DarkscapeTransferWindow()
+        self.ui.setupUi(self)    
+        self.skill_ui = {
+            "attack" : self.ui.attack_level,
+            "strength" : self.ui.strength_level,
+            "defence" : self.ui.defence_level,
+            "ranged" : self.ui.range_level,
+            "prayer" : self.ui.prayer_level,
+            "magic" : self.ui.magic_level,
+            "runecrafting" : self.ui.runecrafting_level,
+            "construction" : self.ui.construction_level,
+            "dungeoneering" : self.ui.dungeoneering_level,
+            "constitution" : self.ui.constitution_level,
+            "agility" : self.ui.agility_level,
+            "herblore" : self.ui.herblore_level,
+            "thieving" : self.ui.thieving_level,
+            "crafting" : self.ui.crafting_level,
+            "fletching" : self.ui.fletching_level,
+            "slayer" : self.ui.slayer_level,
+            "hunter" : self.ui.hunter_level,
+            "divination" : self.ui.divination_level,
+            "mining" : self.ui.mining_level,
+            "smithing" : self.ui.smithing_level,
+            "fishing" : self.ui.fishing_level,
+            "cooking" : self.ui.cooking_level,
+            "firemaking" : self.ui.firemaking_level,
+            "woodcutting" : self.ui.woodcutting_level,
+            "farming" : self.ui.farming_level,
+            "summoning" : self.ui.summoning_level
+        }
+        self.init_validators()
+        self.ui.action_about.triggered.connect(self.about_event)
+        self.ui.action_exit.triggered.connect(QCoreApplication.instance().quit)
+        self.ui.get_stats_button.clicked.connect(self.get_stats)
+        self.ui.rs3_name.returnPressed.connect(self.get_stats)
+        self.show()
+    def init_validators(self):
+        regex = QRegExp('^[1-9][0-9]$')
+        self.skill_validator = QRegExpValidator(regex)
+        for skill, ui_level in self.skill_ui.items():
+            ui_level.setValidator(self.skill_validator)
+        regex = QRegExp('[0-9a-zA-Z\s]+')
+        self.name_validator = QRegExpValidator(regex)
+        self.ui.rs3_name.setValidator(self.name_validator)
+
+    def about_event(self):
+        QMessageBox.about(
+            self,
+            "About",
+            "Made by Brbwipe for the rsdarkscape subreddit"
+        )
+
+    def get_level(self, skill):
+        this_skill = self.skill_dictionary[skill]
+        return this_skill.text
+
+    def get_stats(self):
+        rs3_name = self.ui.rs3_name.text()
+        runescaper = Runescaper(rs3_name)
+        skills_dict = {}
+        for skill, ui_level in self.skill_ui.items():
+            skill_level = ui_level.text() if ui_level.text() else "1"
+            skills_dict[skill] = int(skill_level)
+        print(skills_dict)
+        runescaper.add_skill_dict(skills_dict)
+        QMessageBox.about(
+            self,
+            "Your Stats",
+            str(runescaper)
+        )
+        
+app = QApplication(sys.argv)
+window = DarkscapeUI()
+sys.exit(app.exec_())
---
+++ darkscape_transfer_skills.py
@@ -0,0 +1,123 @@
+import requests
+import math
+
+class Runescaper:
+
+    def __init__(self, name, RS3=True):
+        self.name = name
+        self.skills = [ Skill(x, 1, 0) for x in skill_list ]
+        self.xp_table = [0]
+        points = 0
+        for level in range(1,99):
+            diff = int(level + 300 * math.pow(2, float(level)/7) )
+            points += diff
+            self.xp_table.append(points/4)
+        self.RS3 = self.get_RS3_stats()
+
+    def get_RS3_stats(self):
+        underscored_name = self.name.replace(" ", "_")
+        r = requests.get('http://hiscore.runescape.com/index_lite.ws?player='+underscored_name)
+        ranks_list = []
+        rs3player = (r.status_code != 404)
+        if(rs3player):
+            ranks_list = r.text.split("\n")
+            ranks_list = ranks_list[1:len(skill_list)+1]
+            ranks_list = [stats.split(",") for stats in ranks_list]
+            ranks_list = [[int(stat) for stat in stats] for stats in ranks_list]
+            for val, stats in enumerate(ranks_list, start=0):
+                if(stats[2] == -1):
+                    stats[2] = 0
+                self.change_skill_exp(skill_list[val], stats[2])
+        return rs3player
+
+    def get_level(self, level):
+        return self.xp_table[level-1]
+
+    def add_skill_dict(self, dictionary, level_mode=True):
+        if(level_mode): 
+            for skill, level in dictionary.items():
+                self.change_skill_exp(skill, (self.xp_table[level-1])/2, True)
+        else:
+            for skill, exp in dictionary.items():
+                self.change_skill_exp(skill, exp/2, True)
+
+    def __str__(self):
+        long_string =      "===========Stats for "+self.name+"===========\n\n"
+        if(self.RS3):
+            long_string += "You have an RS3 account. Your exp was added.\n\n"
+        else:
+            long_string += "You don't have an RS3 account. \n\n"
+        for skill in self.skills:
+            long_string += skill.name.title()+": "+str(skill.exp)+" Experience (Level "+str(skill.level)+")\n\n"
+        long_string += "Your total level will be "+str(self.total_level())+"."
+        return long_string
+        
+    def change_skill_exp(self, skill, exp, addition=False):
+        a_skill = list(filter(lambda x: x.name == skill, self.skills))
+        if(a_skill):
+            a_skill[0].change_exp(exp, addition)
+
+    def total_level(self):
+        total_level = 0
+        for skill in self.skills:
+            total_level += skill.level
+        return total_level
+
+class Skill:
+
+    def __init__(self, name, level, exp):
+        self.name = name
+        self.level = level
+        self.exp = int(exp)
+        self.xp_table = [0]
+        points = 0
+        for level in range(1,99):
+            diff = int(level + 300 * math.pow(2, float(level)/7) )
+            points += diff
+            self.xp_table.append(points/4)
+
+    def __str__(self):
+        return self.name+" Level "+str(self.level)+ " XP: "+str(self.exp)
+
+    def change_exp(self, exp, addition=False):
+        self.exp = (self.exp + exp if addition else exp)
+        self.level = self.calculate_level()
+
+    def calculate_level(self):
+        level = 0
+        for x in (self.xp_table):
+            level = level + 1
+            if(self.exp <= x):
+                level = level - 1
+                break
+        return level
+
+skill_list = [
+    "attack",
+    "defence",
+    "strength",
+    "constiution",
+    "ranged",
+    "prayer",
+    "magic",
+    "cooking",
+    "woodcutting",
+    "fletching",
+    "fishing",
+    "firemaking",
+    "crafting",
+    "smithing",
+    "mining",
+    "herblore",
+    "agility",
+    "thieving",
+    "slayer",
+    "farming",
+    "runecrafting",
+    "hunter",
+    "construction",
+    "summoning",
+    "dungeoneering",
+    "divination",
+]
+    
